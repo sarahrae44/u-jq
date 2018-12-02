4ce1bc80aebbc9a8d2d7c87fc5d7483c9726696c
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2018-12-02 13:33:36 -0700

    traversing

---
+++ 03.03.traversing/js/script.js
@@ -0,0 +1,15 @@
+$(function () {
+  // $("#list").find("li").css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("#list").children("li").css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("#list").parents("div").css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("#list").parents("body").css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("#list").parent().css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("#list").siblings().css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("#list").siblings("p").css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("#list").siblings(":header").css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("#list").prev().css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("#list").next().css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $(":header").next().css("background-color", "rgba(180, 180, 30, 0.8)");
+  $("form").children("input").css("background-color", "rgba(180, 180, 30, 0.8)");
+
+});
